c1c8e298f2d28e7938492ead08836d9b7e4803c7
AmaruMoyaR <amaru.moya.ragal@gmail.com>
2023-06-26 09:41:14 -0400

    actualización

--- MovingStates.py
+++ MovingStates.py
@@ -5,8 +5,9 @@ from qutip import *
 # import qutip as q
 import matplotlib.pyplot as plt
 from States import *
-
 N = 40
+
+# %%
 psi_inicial_fock = q.basis(N,10) 
 evolucion_temporal_estado_Fock, tiempo, tasa_inversion = Hamiltonian_2levels(psi_inicial_fock, N , 300)
 
@@ -39,4 +40,54 @@ ax.set_ylabel('Entropia')
 ax.set_title('Entropia de Von Neumann')
 
 
+# %%
+psi_inicial_cat = (coherent(N, -3.0) + coherent(N, 3.0)) / np.sqrt(2)
+evolucion_temporal_estado_cat, tiempo, tasa_inversion = Hamiltonian_2levels(psi_inicial_cat, N , 300)
+
+wignerslist = []
+xvec = np.linspace(-10,10,300)
+
+Wigners = WignerEvolution(evolucion_temporal_estado_cat, xvec, wignerslist)
+
+#%%
+AnimatedWigner(Wigners,xvec,'wignercat')
+
+AnimatePopulation(tasa_inversion,tiempo,'populationwignercat')
+
+# %%
+Entropy = []
+for i in range(len(evolucion_temporal_estado_cat)):
+    Entropy.append(entropy_vn(evolucion_temporal_estado_cat[i]))
+
+fig, ax = plt.subplots(figsize=(6, 6))    
+ax.plot(tiempo,Entropy , color = 'firebrick')
+ax.set_xlabel('Tiempo')
+ax.set_ylabel('Entropia')
+ax.set_title('Entropia de Von Neumann')
+
+# %%
+psi_inicial_Sq = q.displace(N, 2) * q.squeeze(N, 1.0)* q.basis(N,0) 
+evolucion_temporal_estado_Sq, tiempo, tasa_inversion = Hamiltonian_2levels(psi_inicial_Sq, N , 300)
+
+wignerslist = []
+xvec = np.linspace(-10,10,300)
+
+Wigners = WignerEvolution(evolucion_temporal_estado_Sq, xvec, wignerslist)
+
+
+# %%
+AnimatedWigner(Wigners,xvec,'wignersq')
+
+AnimatePopulation(tasa_inversion,tiempo,'populationwignersq')
+
+# %%
+Entropy = []
+for i in range(len(evolucion_temporal_estado_Sq)):
+    Entropy.append(entropy_vn(evolucion_temporal_estado_Sq[i]))
+
+fig, ax = plt.subplots(figsize=(6, 6))    
+ax.plot(tiempo,Entropy , color = 'firebrick')
+ax.set_xlabel('Tiempo')
+ax.set_ylabel('Entropia')
+ax.set_title('Entropia de Von Neumann')
 # %%
