6bbe705ca61e33f6ee0ac420524b2a36927c04b2
AmaruMoyaR <amaru.moya.ragal@gmail.com>
2023-06-27 01:42:50 -0400

    wigner states ready hopefully

--- MovingStates.py
+++ MovingStates.py
@@ -9,32 +9,24 @@ N = 40
 
 # %%
 psi_inicial_fock = q.basis(N,10) 
-evolucion_temporal_estado_Fock, tiempo, tasa_inversion = Hamiltonian_2levels(psi_inicial_fock, N , 300)
+evolucion_temporal_estado_Fock, tiempo, tasa_inversionf = Hamiltonian_2levels(psi_inicial_fock, N , 300)
 
-wignerslist = []
+wignerslistf = []
 xvec = np.linspace(-10,10,300)
 
-Wigners = WignerEvolution(evolucion_temporal_estado_Fock, xvec, wignerslist)
-
-
-# %%
-
-AnimatedWigner(Wigners,xvec,'wignerfock')
-
-AnimatePopulation(tasa_inversion,tiempo,'populationwignerfock')
+Wignersf = WignerEvolution(evolucion_temporal_estado_Fock, xvec, wignerslistf)
 
 # %%
-
-# Entropy = VonEntropy(Wigners,tiempo)
-
+AnimatedWigner(Wignersf,xvec,'wignerfock')
+AnimatePopulation(tasa_inversionf,tiempo,'populationwignerfock')
 
 # %%
-Entropy = []
+Entropyf = []
 for i in range(len(evolucion_temporal_estado_Fock)):
-    Entropy.append(entropy_vn(evolucion_temporal_estado_Fock[i]))
+    Entropyf.append(entropy_vn(evolucion_temporal_estado_Fock[i]))
 
 fig, ax = plt.subplots(figsize=(6, 6))    
-ax.plot(tiempo,Entropy , color = 'firebrick')
+ax.plot(tiempo,Entropyf , color = 'firebrick')
 ax.set_xlabel('Tiempo')
 ax.set_ylabel('Entropia')
 ax.set_title('Entropia de Von Neumann')
@@ -42,52 +34,85 @@ ax.set_title('Entropia de Von Neumann')
 
 # %%
 psi_inicial_cat = (coherent(N, -3.0) + coherent(N, 3.0)) / np.sqrt(2)
-evolucion_temporal_estado_cat, tiempo, tasa_inversion = Hamiltonian_2levels(psi_inicial_cat, N , 300)
-
-wignerslist = []
+evolucion_temporal_estado_cat, tiempo, tasa_inversionc = Hamiltonian_2levels(psi_inicial_cat, N , 300)
+wignerslistc = []
 xvec = np.linspace(-10,10,300)
-
-Wigners = WignerEvolution(evolucion_temporal_estado_cat, xvec, wignerslist)
-
+Wignersc = WignerEvolution(evolucion_temporal_estado_cat, xvec, wignerslistc)
 #%%
-AnimatedWigner(Wigners,xvec,'wignercat')
-
-AnimatePopulation(tasa_inversion,tiempo,'populationwignercat')
-
+AnimatedWigner(Wignersc,xvec,'wignercat')
+AnimatePopulation(tasa_inversionc,tiempo,'populationwignercat')
 # %%
-Entropy = []
+Entropyc = []
 for i in range(len(evolucion_temporal_estado_cat)):
-    Entropy.append(entropy_vn(evolucion_temporal_estado_cat[i]))
+    Entropyc.append(entropy_vn(evolucion_temporal_estado_cat[i]))
 
 fig, ax = plt.subplots(figsize=(6, 6))    
-ax.plot(tiempo,Entropy , color = 'firebrick')
+ax.plot(tiempo,Entropyc , color = 'firebrick')
 ax.set_xlabel('Tiempo')
 ax.set_ylabel('Entropia')
 ax.set_title('Entropia de Von Neumann')
 
-# %%
-psi_inicial_Sq = q.displace(N, 2) * q.squeeze(N, 1.0)* q.basis(N,0) 
-evolucion_temporal_estado_Sq, tiempo, tasa_inversion = Hamiltonian_2levels(psi_inicial_Sq, N , 300)
 
-wignerslist = []
+# %%
+psi_inicial_Sq =  q.squeeze(N, 1.0)* q.basis(N,0) 
+evolucion_temporal_estado_Sq, tiempo, tasa_inversionsq = Hamiltonian_2levels(psi_inicial_Sq, N , 300)
+
+wignerslistsq = []
 xvec = np.linspace(-10,10,300)
 
-Wigners = WignerEvolution(evolucion_temporal_estado_Sq, xvec, wignerslist)
-
-
+Wignerssq = WignerEvolution(evolucion_temporal_estado_Sq, xvec, wignerslistsq)
 # %%
-AnimatedWigner(Wigners,xvec,'wignersq')
-
-AnimatePopulation(tasa_inversion,tiempo,'populationwignersq')
-
+AnimatedWigner(Wignerssq,xvec,'wignersq')
+AnimatePopulation(tasa_inversionsq,tiempo,'populationwignersq')
 # %%
-Entropy = []
+Entropysq = []
 for i in range(len(evolucion_temporal_estado_Sq)):
-    Entropy.append(entropy_vn(evolucion_temporal_estado_Sq[i]))
+    Entropysq.append(entropy_vn(evolucion_temporal_estado_Sq[i]))
 
 fig, ax = plt.subplots(figsize=(6, 6))    
-ax.plot(tiempo,Entropy , color = 'firebrick')
+ax.plot(tiempo,Entropysq , color = 'firebrick')
 ax.set_xlabel('Tiempo')
 ax.set_ylabel('Entropia')
 ax.set_title('Entropia de Von Neumann')
+
+
+# %%
+psi_inicial_Sq_c =  (q.displace(N,-1)*q.squeeze(N, 1.0)*q.basis(N,0) + q.displace(N,1)*q.squeeze(N, 1.0)*q.basis(N,0)).unit()
+evolucion_temporal_estado_Sq_c, tiempo, tasa_inversionsq_c = Hamiltonian_2levels(psi_inicial_Sq_c, N , 300)
+wignerslistsq_c = []
+xvec = np.linspace(-10,10,300)
+Wignerssq_c = WignerEvolution(evolucion_temporal_estado_Sq_c, xvec, wignerslistsq_c)
+# %%
+AnimatedWigner(Wignerssq_c,xvec,'wignersq_c')
+AnimatePopulation(tasa_inversionsq_c,tiempo,'populationwignersq_c')
+
+# %%
+Entropysq_c = []
+for i in range(len(evolucion_temporal_estado_Sq_c)):
+    Entropysq_c.append(entropy_vn(evolucion_temporal_estado_Sq_c[i]))
+
+fig, ax = plt.subplots(figsize=(6, 6))    
+ax.plot(tiempo,Entropysq_c , color = 'firebrick')
+ax.set_xlabel('Tiempo')
+ax.set_ylabel('Entropia')
+ax.set_title('Entropia de Von Neumann')
+
+# %%
+f = open('wigner.txt','w')
+f.write(str(Wignersf))
+f.write('\n')
+f.write(str(wignerslistsq))
+f.write('\n')
+f.write(str(Wignersc))
+f.write('\n')
+f.write(str(wignerslistc))
+f.write('\n')
+f.write(str(Wignerssq))
+f.write('\n')
+f.write(str(wignerslistsq))
+f.write('\n')
+f.write(str(Wignerssq_c))
+f.write('\n')
+f.write(str(wignerslistsq_c))
+f.close()
 # %%
